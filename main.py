e449c2de4302de1955f800666d2f2b3c0f8afede
marcos <marcosoliveira3@aluno.uema.br>
2022-07-26 23:17:03 -0300

    draw do objeto selecionado

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pygame
 from pygame.locals import *
 
+from math import *
+
 import OpenGL
 from OpenGL.GL import *
 from OpenGL.GLUT import *
@@ -20,6 +22,38 @@ def quadrado():
         for vertex in e:
             glVertex2iv(vertices[vertex])
     glEnd()
+'''
+Para tranferir o círculo de lugar
+x = R * cos (angle) + X_centre
+y = R * sin(angle)  + Y_centre
+
+posx, posy = 0, 0
+sides = 32
+radius = 1
+'''
+def circulo(posx, posy, sides, radius):
+    glBegin(GL_LINES)
+    for i in range(100):
+        cosine = radius * cos(i * 2 * pi / sides) + posx
+        sine = radius * sin(i * 2 * pi / sides) + posy
+        glVertex2d(cosine, sine)
+    glEnd()
+
+def parabola(posx, posy, sides, radius):
+    glBegin(GL_LINES)
+    for i in range(33):
+        cosine = radius * cos(i * pi / sides) + posx
+        sine = radius * sin(i * pi / sides) + posy
+        glVertex2d(cosine, -sine)
+    glEnd()
+
+def parabola2(posx, posy, sides, radius):
+    glBegin(GL_LINES)
+    for i in range(33):
+        cosine = radius * cos(i * pi / sides) + posx
+        sine = radius * sin(i * pi / sides) + posy
+        glVertex2d(cosine, sine)
+    glEnd()
 
 #CRIANDO JANELA DE VISUALIZAÇÃO
 def main():
@@ -35,7 +69,12 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        quadrado()
+        circulo(0,0,64,1)
+        parabola(0, 0.1 , 32, 0.5)
+        circulo(0.5, -0.1, 64, 0.1)
+        circulo(-0.5, -0.1, 64, 0.1)
+        parabola2(0.25, 0.2, 32, 0.15)
+        parabola2(-0.25, 0.2, 32, 0.15)
         pygame.display.flip()
 
 main()
